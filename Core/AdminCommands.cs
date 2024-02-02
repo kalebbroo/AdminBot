8bf411613f59fc311fa8a59e5788f5fedd77b697
kalebbroo <kalebbroo@gmail.com>
2024-02-02 16:51:52 -0500

    actually send the modal and made it in the proper structure.

--- Core/AdminCommands.cs
+++ Core/AdminCommands.cs
@@ -1,14 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Discord.Commands;
 using Discord.Interactions;
-using Discord.WebSocket;
 using Discord;
-using Microsoft.VisualBasic;
-using Discord.Rest;
 
 
 namespace AdminBot.Core
@@ -20,17 +11,41 @@ namespace AdminBot.Core
         [SlashCommand("setup_rules", "Sets up the rules for the server")]
         public async Task SetupRules()
         {
-            await DeferAsync(ephemeral: true);
             // Define the text channel with the name "rules"
             var RulesChannel = Context.Guild.TextChannels.FirstOrDefault(x => x.Name == "rules");
 
             if (RulesChannel is null)
             {
-                // If the channel does not exist, create it
+                // If the channel does not exist, create it and make that channel the RulesChannel variable
                 await Context.Guild.CreateTextChannelAsync("rules");
+                RulesChannel = Context.Guild.TextChannels.FirstOrDefault(x => x.Name == "rules");   
             }
-            // Send the modal to the user
-            await FollowupAsync("test the command", ephemeral: true);
+
+            // Get the previous message so admins can make small edits instead of retyping the whole thing
+            // TODO: Add and retrieve message from DB, Right now it assumes there will only be 1 message in the channel.
+            var messages = await RulesChannel.GetMessagesAsync(1).FlattenAsync();
+            var message = messages.FirstOrDefault();
+
+            // TODO: Fix this so there is no reused code. Maybe define the default text before the if?
+            if (message is null)
+            {
+                // Create default text to put in the modal. It just looks better..
+                string LameTitle = "Server Rules",
+                    LameDescription = "No Spamming";
+                var rulesModal = new RulesModal(LameTitle, LameDescription);
+                // Send the modal to the user
+                await RespondWithModalAsync<RulesModal>("setup_rules_modal", rulesModal);
+            }
+            else
+            {
+                // Extract the title and description from the embed
+                var embed = message.Embeds.FirstOrDefault();
+                string LameTitle = embed.Title,
+                    LameDescription = embed.Description;
+                var rulesModal = new RulesModal(LameTitle, LameDescription);
+                // Send the modal to the user
+                await RespondWithModalAsync<RulesModal>("setup_rules_modal", rulesModal);
+            }
 
         }
 
@@ -38,18 +53,22 @@ namespace AdminBot.Core
         public class RulesModal : IModal
         {
             // Create the modal add each field to make the rules embed
-            public string Title => "Create Server Rules";
+            [InputLabel("Title")]
+            [ModalTextInput("title_input", TextInputStyle.Paragraph,
+            placeholder: "Enter the title", maxLength: 100)]
+            public string? Title { get; set; }
 
             [InputLabel("Description")]
             [ModalTextInput("description_input", TextInputStyle.Paragraph,
-            placeholder: "Enter a brief description", maxLength: 300)]
+            placeholder: "Enter a brief description", maxLength: 1800)]
             public string? Description { get; set; }
             // Constructors
             public RulesModal() { /* ... */ }
-            public RulesModal(string description)
+            public RulesModal(string LameTitle, string LameDescription)
             {
                 // Initialize with provided values
-                Description = description;
+                Title = LameTitle;
+                Description = LameDescription;
             }
         }
 
@@ -66,25 +85,32 @@ namespace AdminBot.Core
         [SlashCommand("test_embed", "Tests the embed builder")]
         public async Task TestEmbed()
         {
-            // Create a new embed
-            var parameters = new Dictionary<string, string>
+            try
             {
-                { "title", "My Title" },
-                { "description", "My Description" },
-                { "url", "https://example.com" },
-                { "thumbnail", "https://example.com/thumbnail.png" },
-                { "image", "https://example.com/image.png" },
-                { "author", "My Author" },
-                { "color", "FF5733" },
-                { "footer", "My Footer" },
-                { "timestamp", "2022-01-01T00:00:00+00:00" },
-                { "field", "Field 1|Value 1" },
-                { "field", "Field 2|Value 2" },
-            };
-            var embed = EmbedCreator.CreateEmbed(parameters);
+                // Create a new embed
+                var parameters = new Dictionary<string, string>
+                {
+                    { "title", "My Title" },
+                    { "description", "My Description" },
+                    { "url", "https://example.com" },
+                    { "thumbnail", "https://example.com/thumbnail.png" },
+                    { "image", "https://example.com/image.png" },
+                    { "author", "My Author" },
+                    { "color", "FF5733" },
+                    { "footer", "My Footer" },
+                    { "timestamp", "2022-01-01T00:00:00+00:00" },
+                    { "field", "Field 1|Value 1" },
+                    { "field", "Field 2|Value 2" },
+                };
+                var embed = EmbedCreator.CreateEmbed(parameters);
 
-            // Send the embed
-            await RespondAsync(embed: embed);
+                // Send the embed
+                await RespondAsync(embed: embed);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
 
         [SlashCommand("ping", "Test command")]
