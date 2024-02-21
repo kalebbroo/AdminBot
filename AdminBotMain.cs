18bbf0c061b368be006b109b55ab0777ee9589f1
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-02-21 03:14:37 -0500

    Add dependency injection and fix TTS command

--- AdminBotMain.cs
+++ AdminBotMain.cs
@@ -4,6 +4,9 @@ using Discord.Interactions;
 using dotenv.net;
 using System.Reflection;
 using AdminBot.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using OpenAI_API.Embedding;
 
 namespace AdminBot
 { 
@@ -13,6 +16,8 @@ namespace AdminBot
         private DiscordSocketClient? _client;
         private InteractionService? _interactions;
         private Database? _database;
+
+        private IServiceProvider _serviceProvider;
 
         public static Task Main(string[] args) => new AdminBotMain().MainAsync();
 
@@ -33,23 +38,34 @@ namespace AdminBot
 
             // Create a new instance of DiscordSocketClient, pass in config.
             _client = new DiscordSocketClient(config);
-            // Initialize the Database
-            _database = new Database();
-            // Create a new instance of InteractionService, pass in _client. Needed for interactions.
-            _interactions = new InteractionService(_client);
+
+            // Instead of initializing all our services as variables here, we will instead use dependency injection. 
+            // This allows us to have singleton instances of our services that can be used across the application.
+            // The library is desinged to work well with this and services and modules will automatically be popualted
+            // With the services that they want just be referencing them as a readonly variable.
+            _serviceProvider = new ServiceCollection()
+                .AddSingleton<DiscordSocketClient>(_client)
+                .AddSingleton<XpLevels>()
+                .AddSingleton<Database>()
+                .AddSingleton<Listeners>()
+                .AddSingleton<VoiceService>()
+                .AddSingleton<InteractionService>()
+                .BuildServiceProvider();
+
+            _interactions = _serviceProvider.GetRequiredService<InteractionService>();
 
             // Add the event to the client's InteractionCreated event.
             // This event is triggered when a user interacts with a slash command.
             _client.InteractionCreated += async interaction =>
             {
                 var ctx = new SocketInteractionContext(_client, interaction);
-                await _interactions.ExecuteCommandAsync(ctx, services: null);
+                await _interactions.ExecuteCommandAsync(ctx, services: _serviceProvider);
             };
 
             // Instantiate your Listeners class and register the listeners
-            var database = new Database();
-            var listeners = new Listeners(_client, database);
-            var xpLevels = new XpLevels(_client, database);
+            var database = _serviceProvider.GetRequiredService<Database>();
+            var listeners = _serviceProvider.GetRequiredService<Listeners>();
+            var voice = _serviceProvider.GetRequiredService<VoiceService>();
             listeners.RegisterListeners();
 
             // Add the Log and ReadyAsync methods to the client's Log and Ready events.
@@ -87,7 +103,8 @@ namespace AdminBot
                 {
                     // Register command modules with the InteractionService.
                     // Tels it to scan the whole assembly for classes that define slash commands.
-                    await _interactions.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+                    // Also pass in the service provider so services can be accessed in modules
+                    await _interactions.AddModulesAsync(Assembly.GetEntryAssembly(), _serviceProvider);
 
                     // Get the ID of the first guild the bot is a member of
                     // Then register the commands to that guild
